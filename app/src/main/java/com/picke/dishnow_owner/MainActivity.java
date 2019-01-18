7a456b424a07a26a6cff704bdbd42fe8d5cde538
성빈 <spark@spark-Mac.local>
2019-01-18 22:54:03 +0900

    spark

--- app/src/main/java/com/picke/dishnow_owner/MainActivity.java
+++ app/src/main/java/com/picke/dishnow_owner/MainActivity.java
@@ -13,6 +13,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -41,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
     private Socket mSocket;
     private Button button;
     private EditText editText;
-    private String str;
     private TextView tv;
     Handler handler = null;
 
@@ -49,17 +51,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GetData task = new GetData();
+        //GetData task = new GetData();
         //task.execute(feed_url,"1");
         button = findViewById(R.id.main_button);
         editText = findViewById(R.id.main_chat);
         tv = findViewById(R.id.main_tv);
 
-        try{
-            mSocket= IO.socket("http://ec2-18-218-206-167.us-east-2.compute.amazonaws.com:3000");
+        try {
+            mSocket = IO.socket("http://ec2-18-218-206-167.us-east-2.compute.amazonaws.com:3000");
+            mSocket.on(Socket.EVENT_CONNECT, (Object... objects) -> {
+
+            }).on("recMsg", (Object... objects) -> {
+                JsonParser jsonParsers = new JsonParser();
+                JsonObject jsonObject = (JsonObject) jsonParsers.parse(objects[0] + "");
+                runOnUiThread(() -> {
+                    tv.setText(tv.getText().toString() + jsonObject.get("message").getAsString());
+                });
+            });
             mSocket.connect();
-           // mSocket.on("chat message",onMessageReceived);
-        }catch (URISyntaxException e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -75,137 +85,25 @@ public class MainActivity extends AppCompatActivity {
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
-                        mSocket.on("chat message",onMessageReceived);
                     }
                 });
             }
         });
         //thread.start();
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mSocket.emit("chat message",editText.getText().toString());
+        button.setOnClickListener((view)->{
+            JsonObject preJsonobject = new JsonObject();
+            preJsonobject.addProperty("message",editText.getText().toString());
+            JSONObject jsonObject = null;
+            try{
+                jsonObject = new JSONObject(preJsonobject.toString());
+            }catch(JSONException e){
+                e.printStackTrace();
             }
+            mSocket.emit("msg",jsonObject);
+            editText.setText("");
         });
-    }
-
-    // 서버로부터 전달받은 'chat-message' Event 처리.
-    private Emitter.Listener onMessageReceived = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            // 전달받은 데이터는 아래와 같이 추출할 수 있습니다.
-            String receivedData = (String) args[0];
-            tv.setText(receivedData);
-            //Log.d("asd",receivedData.getString("chat message"));
-            //tv.setText(receivedData.getString("chat message"));
-
-        }
-    };
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        GetData task = new GetData();
-        //task.execute(feed_url,"1");
-
-    }
-
-    public class GetData extends AsyncTask<String, Void, String> {
-        //ProgressDialog progressDialog;
-        String target;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            // progressDialog = ProgressDialog.show(ShowObectActivity.this,
-            //      "Please Wait", null, true, true);
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-                try {
-                    Thread.sleep(1000);
-                    URL url = new URL(params[0]);
-                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                    httpURLConnection.setReadTimeout(10000);
-                    httpURLConnection.setConnectTimeout(10000);
-
-                    httpURLConnection.setRequestMethod("POST");
-                    httpURLConnection.setDoInput(true);
-                    httpURLConnection.connect();
-                    //수정
-                    String mres = params[1];
-
-                    String data = "res_id=" + mres;
-
-                    OutputStream outputStream = httpURLConnection.getOutputStream();
-                    outputStream.write(data.getBytes("UTF-8"));
-                    outputStream.flush();
-                    outputStream.close();
-                    //int responseStatusCode = httpURLConnection.getResponseCode();
-                    Log.d(TAG, data);
-
-                    InputStream inputStream = httpURLConnection.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-                    StringBuilder sb = new StringBuilder();
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        sb.append(line + "\n");
-                    }
-                    bufferedReader.close();
-                    inputStream.close();
-                    return sb.toString().trim();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    Log.d(TAG, "DOIN", e);
-                }
-                return null;
-        }
-
-        @Override
-        public void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-//            progressDialog.dismiss();
-//            Log.d(TAG, result);
-            mJsonString = result;
-            if (mJsonString == null) Log.d(TAG, "null!!!");
-            showResult();
         }
     }
 
-    public void showResult() {
-        String TAG_JSON = "response";
-        try {
-            JSONObject jsonObject = new JSONObject(mJsonString);
-            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
 
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject item = jsonArray.getJSONObject(i);
-                String userid = item.getString("userid");
-                String numbers = item.getString("numbers");
-                start="start";
-                String tmp = start;
-                start = userid + numbers;
-                if (start != tmp) {
-                    intent = new Intent(MainActivity.this, PopupActivity.class);
-                    intent.putExtra("user_id", userid);
-                    intent.putExtra("numbers", numbers);
-                    startActivity(intent);
-                    Log.d("claor328",start);
-                    finish();
-                }
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-            //     Log.d(TAG, "showResult : ", e);
-        }
-    }
-}
