020015e9b361fd48cd1f92d4c4048496f185ab3f
unknown <meenseek5929@gmail.com>
2019-02-09 03:06:57 +0900

    meenseek

---
+++ app/src/main/java/com/picke/dishnow_owner/ResInfoActivity.java
@@ -0,0 +1,13 @@
+package com.picke.dishnow_owner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_res_info);
+    }
+}
